179999d9fc83da0d32d3ece6e77767c290b4b3be
Harri Lainio <harri.lainio@op.fi>
2020-11-10 17:22:25 +0200

    removing TLS field from configs and made PKI to pointer

--- main.go
+++ main.go
@@ -27,8 +27,7 @@ func main() {
 		pki.Server.CertFile, pki.Server.KeyFile, pki.Client.CertFile)
 	rpc.Serve(rpc.ServerCfg{
 		Port: 50051,
-		TLS:  *tls,
-		PKI:  *pki,
+		PKI:  pki,
 		Register: func(s *grpc.Server) error {
 			ops.RegisterDevOpsServer(s, &devOpsServer{Root: "findy-root"})
 			glog.V(10).Infoln("GRPC registration all done")
