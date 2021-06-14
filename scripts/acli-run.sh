23822e1aa2213cbba1d3eb64e413e098da183e46
Harri Lainio <harri.lainio@op.fi>
2021-06-14 17:36:25 +0300

    new helper

---
+++ scripts/acli-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+acli \
+	$@ \
+	-logging "-logtostderr -v=3" \
+	-origin https://localhost:8090 \
+	-cert-path $GOPATH/src/github.com/findy-network/findy-common-go/cert \
+	-port 8090
