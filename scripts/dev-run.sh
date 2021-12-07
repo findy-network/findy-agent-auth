6b1f5724cebdf40f694435af451870c06df73aa8
Harri Lainio <harri.lainio@op.fi>
2021-12-07 13:16:19 +0200

    dev run script update for correct agency address and limit backups on tests

--- scripts/dev-run.sh
+++ scripts/dev-run.sh
@@ -2,7 +2,9 @@
 
 go run .. \
 	$@ \
+	-agency "localhost" \
 	-logging "-logtostderr -v=3" \
 	-origin http://localhost:8090 \
 	-cert-path $GOPATH/src/github.com/findy-network/findy-common-go/cert \
+	-sec-backup-interval 100 \
 	-port 8090
