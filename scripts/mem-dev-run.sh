42d96905c66e8486e81d2ccad468ee9c723c08c5
Harri Lainio <harri.lainio@iki.fi>
2023-06-18 20:18:01 +0300

    start script for dev allows no TLS

--- scripts/mem-dev-run.sh
+++ scripts/mem-dev-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 GOPATH=${GOPATH:-`go env GOPATH`}
+CERT_PATH=${CERT_PATH:-""}
+
+echo "CERT_PATH: $CERT_PATH"
 
 # MEMORY_ prefix is a memory db, not saved to file
 enclaveFile="MEMORY_enclave.bolt" 
@@ -10,7 +13,7 @@ go run .. \
 	-agency "localhost" \
 	-logging "-logtostderr -v=3" \
 	-origin http://localhost:8090 \
-	-cert-path $GOPATH/src/github.com/findy-network/findy-agent/grpc/cert \
+	-cert-path=$CERT_PATH \
 	-sec-file "$enclaveFile" \
 	-sec-backup-interval 100 \
 	-port 8090
